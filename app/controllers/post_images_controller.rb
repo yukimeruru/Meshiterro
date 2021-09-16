c1c3369cc733859bea1b695fa5e941035e976e1b
EC2 Default User <ec2-user@ip-172-31-34-255.ec2.internal>
2021-09-16 13:58:21 +0000

    [add2]meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,11 +11,13 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+     @post_images = PostImage.page(params[:page]).reverse_order
   end
 
   def show
     @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+
   end
 
   def destroy
